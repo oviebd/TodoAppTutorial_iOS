d9744a648aff7e41e0b7852eeb8b8b5dcc2d9b6d
Habibur_Periscope <habibur.rahman@periscopelabs.io>
2025-07-26 09:16:02 +0600

    create a new project

---
+++ TodoApp_Tutorial/TodoApp_Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoApp_Tutorial
+//
+//  Created by Habibur_Periscope on 26/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoApp_Tutorial/TodoApp_Tutorial/TodoApp_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp_TutorialApp.swift
+//  TodoApp_Tutorial
+//
+//  Created by Habibur_Periscope on 26/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
